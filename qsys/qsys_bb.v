5f526a002b35110ba69dbf07a810e8ac14534e3f
Nick Rosencrantz <nik@kth.se>
2017-04-11 17:18:51 +0200

    added

---
+++ qsys/qsys_bb.v
@@ -0,0 +1,30 @@
+
+module qsys (
+	clk_clk,
+	reset_reset_n,
+	sdram_wire_addr,
+	sdram_wire_ba,
+	sdram_wire_cas_n,
+	sdram_wire_cke,
+	sdram_wire_cs_n,
+	sdram_wire_dq,
+	sdram_wire_dqm,
+	sdram_wire_ras_n,
+	sdram_wire_we_n,
+	uart_external_connection_rxd,
+	uart_external_connection_txd);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output	[12:0]	sdram_wire_addr;
+	output	[1:0]	sdram_wire_ba;
+	output		sdram_wire_cas_n;
+	output		sdram_wire_cke;
+	output		sdram_wire_cs_n;
+	inout	[31:0]	sdram_wire_dq;
+	output	[3:0]	sdram_wire_dqm;
+	output		sdram_wire_ras_n;
+	output		sdram_wire_we_n;
+	input		uart_external_connection_rxd;
+	output		uart_external_connection_txd;
+endmodule
